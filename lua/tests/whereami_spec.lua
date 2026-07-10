50dd2db9e45b2ea426cde10ed50764614238c982
Reinier Hernández <8838803+ragnarok22@users.noreply.github.com>
2026-07-10 13:20:44 -0300

    Normalize Lua formatting

--- lua/tests/whereami_spec.lua
+++ lua/tests/whereami_spec.lua
@@ -1,41 +1,41 @@
-local stub = require('luassert.stub')
-local whereami = require('whereami')
-local curl = require('plenary.curl')
+local stub = require("luassert.stub")
+local whereami = require("whereami")
+local curl = require("plenary.curl")
 
 -- debug.getupvalue returns the upvalue name and value. We only want the value
 local _, get_flag = debug.getupvalue(whereami.country, 2)
 
-describe('whereami', function()
-  it('returns proper flag emoji', function()
-    assert.is_function(get_flag)
-    assert.are.equal('🇺🇸', get_flag('US'))
-  end)
+describe("whereami", function()
+	it("returns proper flag emoji", function()
+		assert.is_function(get_flag)
+		assert.are.equal("🇺🇸", get_flag("US"))
+	end)
 
-  it('uses fallback icon when get_flag returns empty string', function()
-    local curl_stub = stub(curl, 'get', function()
-      return { body = '{"country":"US"}' }
-    end)
-    local notify_stub = stub(vim, 'notify')
+	it("uses fallback icon when get_flag returns empty string", function()
+		local curl_stub = stub(curl, "get", function()
+			return { body = '{"country":"US"}' }
+		end)
+		local notify_stub = stub(vim, "notify")
 
-    -- replace get_flag upvalue with stub that returns empty string
-    local original = get_flag
-    debug.setupvalue(whereami.country, 2, function()
-      return ''
-    end)
+		-- replace get_flag upvalue with stub that returns empty string
+		local original = get_flag
+		debug.setupvalue(whereami.country, 2, function()
+			return ""
+		end)
 
-    whereami.country()
+		whereami.country()
 
-    assert.stub(vim.notify).was_called_with(
-      'You are in 🌎US',
-      vim.log.levels.INFO,
-      { title = 'Where am I?', icon = '🌎' }
-    )
+		assert.stub(vim.notify).was_called_with(
+			"You are in 🌎US",
+			vim.log.levels.INFO,
+			{ title = "Where am I?", icon = "🌎" }
+		)
 
-    -- revert
-    debug.setupvalue(whereami.country, 2, original)
-    curl_stub:revert()
-    notify_stub:revert()
-  end)
+		-- revert
+		debug.setupvalue(whereami.country, 2, original)
+		curl_stub:revert()
+		notify_stub:revert()
+	end)
 end)
 
 
